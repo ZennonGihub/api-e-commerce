19311d3331ea35b99a81ad2c0bbfe708cacf9b5f
Zennon <zeitaa34@gmail.com>
2025-12-29 11:06:08 -0300

    Cambios y mejoras en la logica del servicio de ordenes

--- src/services/ordenes.service.js
+++ src/services/ordenes.service.js
@@ -1,15 +1,14 @@
 import boom from '@hapi/boom';
 import { models } from '../libs/sequelize.js';
+import { sequelize } from '../libs/sequelize.js';
 
 export class OrderService {
   constructor() {}
 
   async create(userId, idAddress) {
-    // Iniciamos nuestra transaccion
     const transaction = await sequelize.transaction();
 
     try {
-      // Validamos al usuario
       const user = await models.User.findByPk(userId, {
         include: ['customer'],
       });
@@ -18,7 +17,6 @@ export class OrderService {
         throw boom.badRequest('Debes tener un perfil de cliente para comprar');
       }
 
-      // B. Obtener Carrito con Productos
       const cart = await models.Cart.findOne({
         where: { idUser: userId },
         include: [
@@ -33,18 +31,14 @@ export class OrderService {
         throw boom.badRequest('El carrito está vacío');
       }
 
-      // C. CALCULAR TOTAL Y VALIDAR STOCK
       let total = 0;
-
       for (const item of cart.items) {
-        // Validación de Stock antes de procesar
         if (item.product.stock < item.quantity) {
           throw boom.conflict(`Stock insuficiente para: ${item.product.name}`);
         }
         total += item.product.price * item.quantity;
       }
 
-      // Creamos la orden
       const newOrder = await models.Order.create(
         {
           idCustomer: user.customer.id,
@@ -56,49 +50,43 @@ export class OrderService {
       );
 
       for (const item of cart.items) {
-        // Creamos el OrderItem
         await models.OrderItem.create(
           {
             idOrder: newOrder.id,
             idProduct: item.product.id,
             quantity: item.quantity,
-            unitPrice: item.product.price, // Guardamos el precio histórico
+            unitPrice: item.product.price,
           },
           { transaction: transaction },
         );
 
-        // Descontamos el stock del/los producto/s
         await models.Product.update(
           { stock: item.product.stock - item.quantity },
           { where: { id: item.product.id }, transaction: transaction },
         );
       }
 
-      // Limpiamos el carrito
       await models.CartItem.destroy({
         where: { idCart: cart.id },
         transaction: transaction,
       });
 
-      // Confirmamos
       await transaction.commit();
-
       return newOrder;
     } catch (error) {
-      // Por si algo falla, vuelta atras
       await transaction.rollback();
       throw error;
     }
   }
 
-  async addItem(data) {
-    const newItem = await models.OrderItem.create(data);
-    return newItem;
-  }
-
   async find() {
     const orders = await models.Order.findAll({
-      include: ['user'],
+      include: [
+        {
+          association: 'customer',
+          include: ['user'],
+        },
+      ],
     });
     return orders;
   }
@@ -107,8 +95,8 @@ export class OrderService {
     const order = await models.Order.findByPk(id, {
       include: [
         {
-          association: 'user',
-          include: ['customer'],
+          association: 'customer',
+          include: ['user'],
         },
         'items',
       ],
@@ -120,23 +108,23 @@ export class OrderService {
     return order;
   }
 
-  async findUserAllOrders(userId) {
+  async findByUser(userId) {
+    const customer = await models.Customer.findOne({
+      where: { idUser: userId },
+    });
+
+    if (!customer) return [];
     const orders = await models.Order.findAll({
       where: {
-        idUser: userId,
+        idCustomer: customer.id,
       },
-      include: ['items', 'status'],
+      include: ['items'],
     });
     return orders;
   }
 
   async update(id, changes) {
     const order = await this.findOne(id);
-
-    if (!changes) {
-      throw boom.badRequest('No se proporcionó información');
-    }
-
     const rta = await order.update(changes);
     return rta;
   }
